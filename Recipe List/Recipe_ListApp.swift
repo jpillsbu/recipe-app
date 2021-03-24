df63460cb03ec62dbaae4e3be21ac398da549fd1
John Pillsbury <jpillsbu@gmail.com>
2021-03-23 21:16:43 -0400

    Initial Commit

---
+++ Recipe List/Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ListApp.swift
+//  Recipe List
+//
+//  Created by John Pillsbury on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
